45795f8f83db3edc4ac72ed35deba30f2ad3a736
LailaHms <LailaHms>
2018-10-01 11:14:21 +0200

    Bug fixes for launch files

--- scripts/faces_subscriber.py
+++ scripts/faces_subscriber.py
@@ -4,12 +4,21 @@ from openface_ros.msg import Faces
 from openface_ros.msg import Face
 from std_msgs.msg import Int32
 
+pub = rospy.Publisher('/faces_count', Int32, queue_size=10)
+n_faces = 0
+
+def faces_handler():
+    rospy.init_node('faces_subscriber', anonymous=True)
+    rospy.Subscriber('faces', Faces, faces_count_callback)
+    rospy.spin()
 
 def faces_count_callback(msg):
-    rospy.loginfo("Number of faces detected : %d", len(msg.faces)) 
+    n_faces = len(msg.faces)
+    rospy.loginfo("Number of faces detected : %d", n_faces)
+    pub.publish(n_faces)
 
 if __name__=='__main__':
-    rospy.init_node('faces_subscriber')
-    
-    sub=rospy.Subscriber('faces', Faces, faces_count_callback)
-    rospy.spin()
+    try:
+        faces_handler()
+    except rospy.ROSInterruptException:
+        pass
