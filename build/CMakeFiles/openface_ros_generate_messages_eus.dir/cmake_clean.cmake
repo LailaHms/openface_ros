45cb16865ac113de525f166c64263dfd4e3b8fc3
LailaHms <LailaHms>
2018-10-02 09:37:14 +0200

    Added install script for the package as well as for the dependencies of the project

---
+++ build/CMakeFiles/openface_ros_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openface_ros_generate_messages_eus"
+  "devel/share/roseus/ros/openface_ros/msg/ActionUnit.l"
+  "devel/share/roseus/ros/openface_ros/msg/Face.l"
+  "devel/share/roseus/ros/openface_ros/msg/Faces.l"
+  "devel/share/roseus/ros/openface_ros/manifest.l"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/openface_ros_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
