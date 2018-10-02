f8403ca06fe54196b1b70216e4f4ae3d52abcc88
LailaHms <LailaHms>
2018-10-02 10:32:05 +0200

    Added git ignore for cmake files and build folder

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chilirobot/catkin_ws/src/openface_ros
-# Build directory: /home/chilirobot/catkin_ws/src/openface_ros/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
--- install.sh
+++ install.sh
@@ -5,8 +5,20 @@
 # catkin packages.
 #==============================================================================
 
+# Exit script if any command fails
+set -e 
+set -o pipefail
+
+if [ $# -ne 0 ]
+  then
+    echo "Usage: install.sh"
+    exit 1
+fi
+
+
 export PYTHONPATH=/opt/ros/kinetic/lib/python2.7/dist-packages/
-mkdir build && cd build
+mkdir build
+cd build
 cmake ..
 make -j4
 sudo make install -j4
